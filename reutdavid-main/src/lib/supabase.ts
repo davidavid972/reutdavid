bddaf957ee449fe22971c725b9022bf53a7c4d2e
David David <davidd@kan.org.il>
2026-02-24 20:00:56 +0200

    Remove duplicated nested project folder

--- reutdavid-main/src/lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://fjtoxmrosqzgjhesfrui.supabase.co'
-const supabaseKey = 'sb_publishable_KHMadU4tUB03eyCG8kzEAA_pOdYVgw5'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
